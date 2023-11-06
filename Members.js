c8946baa016f35505e53950799f67a9f2030df9f
Dinoo Gunasekera <dinoogunasekera@gmail.com>
2023-11-06 13:10:10 +0530

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,29 @@
+const members = [
+    {
+        id: 1,
+        name:'John Doe',
+        email: 'john@gmail.com',
+        status:'married'
+    },
+    {
+        id: 2,
+        name:'Bob Williams',
+        email: 'bob@gmail.com',
+        status:'Single'
+    },
+    {
+        id: 3,
+        name:'Marry Jane',
+        email: 'marry@gmail.com',
+        status:'married'
+    },
+    {
+        id: 4,
+        name:'Shannon Jackson',
+        email: 'Shannon@gmail.com',
+        status:'single'
+    }
+];
+
+module.exports = members;
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const members = require('./members');
+const logger = require('./middleware/logger')
+
+const app = express();
+
+//initialize middleware
+//app.use(logger);
+
+//gets all the members
+app.get('/api/members', (req,res)=> res.json(members)); 
+
+//get a single member
+app.get('/api/members/:id', (req,res)=>{
+    const found=members.some(member=>member.id===parseInt(req.params.id));
+    if(found){
+    res.json(members.filter(member => member.id===parseInt(req.params.id)));
+
+}else{
+    res.status(403).json({msg: `No member with the id of ${req.params.id} found.`});
+}
+});
+
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>console.log(`Server statrted on port ${PORT}`));
